0736295242543e36789eb1efed26d279a0d615cf
Sophy Tang <34526085+SophyT@users.noreply.github.com>
2018-02-28 02:43:52 +0000

    Update bike_DBSCAN.py

--- bike_DBSCAN.py
+++ bike_DBSCAN.py
@@ -7,22 +7,21 @@ from sklearn.cluster import DBSCAN
 
 data=pd.read_csv("prodata.csv")
 
-#DBSCAN
-db = DBSCAN(eps=2, min_samples=10).fit(data) #在调整参数中多次测试并观察得聚类结果基本稳定在分为两类，eps大约取值在[1,5]时两类，其他一类
+db = DBSCAN(eps=2, min_samples=10).fit(data) 
 core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
 core_samples_mask[db.core_sample_indices_] = True
 labels = db.labels_
 n_clusters_ = len(set(labels))
 numSamples = len(data)
 
-print("类的个数：",n_clusters_)
-print("各点所属的类别：",labels)
+print("number of clusters：",n_clusters_)
+print("labels of points:",labels)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 mark = ['c', 'b', 'g', 'k', 'r', 'm', 'y','#6B8E23','#FFA500', '#CD853F','#FFC0CB', '#BC8F8F']
 
-# 画出所有样例点 属于同一分类的绘制同样的颜色
+#plot all the points.
 for i in range(numSamples):
     ax.scatter(data.iloc[i,0], data.iloc[i,1],data.iloc[i,2],c=mark[labels[i]])
 mark = ['c', 'b', 'g', 'k', 'r', 'm', 'y']
